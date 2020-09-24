f5a9710c8f9a626e6daf8cdab89dcaa6adc8a12e
Julian Salazar <julsal@amazon.com>
2020-09-24 08:35:03 -0700

    Add test for scores from PyTorch BERT

--- tests/test_scorers.py
+++ tests/test_scorers.py
@@ -5,28 +5,33 @@ from mxnet.gluon.data import Dataset
 
 from mlm.loaders import Corpus
 from mlm.models import get_pretrained
-from mlm.scorers import MLMScorer
+from mlm.scorers import MLMScorer, MLMScorerPT
 
 
 
 def _get_scorer_and_corpus():
-    ctxs = [mx.gpu()]
+    ctxs = [mx.cpu()]
     model, vocab, tokenizer = get_pretrained(ctxs, 'bert-base-en-uncased')
-    scorer = MLMScorer(model, vocab, tokenizer, ctxs, eos=True, wwm=False)
+    scorer_mx = MLMScorer(model, vocab, tokenizer, ctxs, eos=True, wwm=False)
+    model, vocab, tokenizer = get_pretrained(ctxs, 'bert-base-uncased')
+    scorer_pt = MLMScorerPT(model, vocab, tokenizer, ctxs, eos=True, wwm=False)
     corpus = Corpus.from_dict({'utt': {'ref': "I am Sam"}})
-    return scorer, corpus
+    return scorer_mx, scorer_pt, corpus
 
 
 def test_mlmscorer_corpus_to_dataset():
-    scorer, corpus = _get_scorer_and_corpus()
-    dataset = scorer.corpus_to_dataset(corpus)
+    scorer_mx, scorer_pt, corpus = _get_scorer_and_corpus()
+    dataset = scorer_mx.corpus_to_dataset(corpus)
     assert isinstance(dataset, Dataset)
     # Our three tokens, plus the EOS
     assert len(dataset) == 4
 
 
 def test_mlmscorer_score():
-    scorer, corpus = _get_scorer_and_corpus()
-    scores, _ = scorer.score(corpus)
+    scorer_mx, scorer_pt, corpus = _get_scorer_and_corpus()
+    scores, _ = scorer_mx.score(corpus)
     assert len(scores) == 1
-    assert pytest.approx(scores[0]) == -13.3065947
+    assert pytest.approx(scores[0], abs=0.0001) == -13.3065947
+    scores, _ = scorer_pt.score(corpus)
+    assert len(scores) == 1
+    assert pytest.approx(scores[0], abs=0.0001) == -13.3065947
